b01308a8cd9a08327075717fb4ae4059c2c52bc4
Mariusz <Mariusz@Mariusz>
2015-09-10 09:43:41 +0200

    Merge composer/master
    Conflicts:
    	Resources/views/Default/Base/index.html.twig

--- CorePrototypeBundle.php
+++ CorePrototypeBundle.php
@@ -8,9 +8,28 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Core\PrototypeBundle\DependencyInjection\Compiler\ConfigPass;
 use Core\PrototypeBundle\DependencyInjection\Compiler\GridBuilderPass;
 
-class CorePrototypeBundle extends Bundle
+class CorePrototypeBundle extends Bundle// implements BundleDependencyInterface
 {
     
+    
+    
+    
+    public function registerDependencies()
+    {
+        return array(
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new APY\DataGridBundle\APYDataGridBundle(),
+            new BeSimple\I18nRoutingBundle\BeSimpleI18nRoutingBundle(),
+            
+            new TMSolution\DataGridBundle\TMSolutionDataGridBundle(),
+            new Core\ModelBundle\CoreModelBundle(),
+            new Core\ClassMapperBundle\CoreClassMapperBundle(),
+ 
+        );
+    }
+    
+               
+               
     public function build(ContainerBuilder $container)
     {
       
