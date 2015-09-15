b874b47aa9edadbc261d27b6b53e569190c8bade
Lukasz_Sobieraj <lukaszsobieraj@aim.com>
2015-09-15 15:43:26 +0200

    Merge origin/master
    Conflicts:
    	Resources/doc/Instalacja.md

---
+++ Resources/public/js/validationtemplate.js
@@ -0,0 +1,26 @@
+(function($){
+    $.validator.setDefaults({
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            var serverError = $('#' + error.attr('id'), element.parent());
+            if (serverError.length > 0) { serverError.remove(); }
+
+            if(element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        },
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        ignore: function(idx, elt) {
+            // We don't validate hidden fields expect if they have rules attached.
+            return $(elt).is(':hidden') && $.isEmptyObject($( this ).rules());
+        }
+    });
+})(jQuery);
