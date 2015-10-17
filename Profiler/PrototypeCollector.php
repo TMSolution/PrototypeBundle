7c555be1eda0d75e13ca2c0010ab06e111bea80d
Jacek Łoziński <Jacek.lozinski@tmsolution.pl>
2015-10-17 21:48:29 +0200

    Merge origin/master

--- Profiler/PrototypeCollector.php
+++ Profiler/PrototypeCollector.php
@@ -15,15 +15,18 @@ use Symfony\Component\Console\Input\ArrayInput;
 /**
  * @author Krzysiek Piasecki <krzysiekpiasecki@gmail.com>
  */
-class PrototypeCollector implements DataCollectorInterface {
+class PrototypeCollector implements DataCollectorInterface
+{
 
     private $data = [];
 
-    public function __construct() {
+    public function __construct()
+    {
         
     }
 
-    public function collect(Request $request, Response $response, Exception $exception = null) {
+    public function collect(Request $request, Response $response, Exception $exception = null)
+    {
         $kernel = new \AppKernel('dev', true);
         $kernel->boot();
         $container = $kernel->getContainer();
@@ -34,80 +37,90 @@ class PrototypeCollector implements DataCollectorInterface {
         $route = $router->match($data["uri"]);
         $data["route"] = $route["_route"];
         $data["controller"] = $route["_controller"];
-        $data["locale"] = $route["_locale"];
-        $data["entityName"] = $route["entityName"];
-        $data["entityClass"] = $classmapper->getEntityClass($route["entityName"], $data["locale"]);
+        if (array_key_exists("_locale", $route)) {
+            $data["locale"] = $route["_locale"];
 
-        $configuratorService = $container->get('prototype.configurator.service');
-        $namesOfServices=$configuratorService->getNamesOfServices();
-        
-        $service = $configuratorService->getService($data["route"], $data["entityClass"]);
-        
-        
-        $data['config'] = $this->printServiceInfo('Config', $configuratorService);
-        $data['twigs'] = $this->printTwigConfig($service->getConfig());
+            $data["entityName"] = $route["entityName"];
+            $data["entityClass"] = $classmapper->getEntityClass($route["entityName"], $data["locale"]);
 
-        $configuratorService = $container->get('prototype.gridconfig.configurator.service');
-        $namesOfServices=$configuratorService->getNamesOfServices();
-        $configuratorService->getService($data["route"], $data["entityClass"]);
-        $data['gridconfig'] = $this->printServiceInfo('Grid', $configuratorService);
+            $configuratorService = $container->get('prototype.configurator.service');
+            $namesOfServices = $configuratorService->getNamesOfServices();
 
-        $configuratorService = $container->get('prototype.formtype.configurator.service');
-        $namesOfServices=$configuratorService->getNamesOfServices();
-        $configuratorService->getService($data["route"], $data["entityClass"]);
-        $data['formtype'] = $this->printServiceInfo('Form Type', $configuratorService);
+            $service = $configuratorService->getService($data["route"], $data["entityClass"]);
 
+
+            $data['config'] = $this->printServiceInfo('Config', $configuratorService);
+            $data['twigs'] = $this->printTwigConfig($service->getConfig());
+
+            $configuratorService = $container->get('prototype.gridconfig.configurator.service');
+            $namesOfServices = $configuratorService->getNamesOfServices();
+            $configuratorService->getService($data["route"], $data["entityClass"]);
+            $data['gridconfig'] = $this->printServiceInfo('Grid', $configuratorService);
+
+            $configuratorService = $container->get('prototype.formtype.configurator.service');
+            $namesOfServices = $configuratorService->getNamesOfServices();
+            $configuratorService->getService($data["route"], $data["entityClass"]);
+            $data['formtype'] = $this->printServiceInfo('Form Type', $configuratorService);
+        }
         $this->data = $data;
     }
 
-    public function getData() {
+    public function getData()
+    {
         return $this->data;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return "prototype";
     }
 
-    protected function printTwigConfig($config) {
+    protected function printTwigConfig($config)
+    {
+
         $output = [];
-        $output[] = ("Twigs: ");
+        $output[] = "<br/><h2>Twigs2</h2>";
+        $output[] = "<table><thead><tr><th>Type</th><th>Twig</th></tr></thead><tbody>";
         foreach ($config as $name => $path) {
-            
-            if(strstr($path,'CorePrototypeBundle:Default')){
-                $output[] = ("<strong>$name</strong>: $path");
+
+            if (strstr($path, 'CorePrototypeBundle:Default')) {
+                $output[] = ("<tr><th>$name</th><td>$path</td></tr>");
+            } else {
+                $output[] = ("<tr><th>$name</th><td><span style='background-color: #aacd4e;border-radius: 6px; color: #fff; display: inline-block;margin-right: 2px;padding: 4px;'>$path</span></td></tr>");
             }
-            else{
-                $output[] = ("<strong>$name</strong>: <span style='background-color: #aacd4e;border-radius: 6px; color: #fff; display: inline-block;margin-right: 2px;padding: 4px;'>$path</span>");
-            }
-            
         }
+        $output[] = "</tbody></table>";
         return $output;
     }
 
-    protected function printServiceInfo($name, $configuratorService) {
+    protected function printServiceInfo($name, $configuratorService)
+    {
         $output = [];
+
 
         $serviceArray = $configuratorService->getChosen();
         if ($name) {
             $output[] = "<br/><h2>" . $name . "</h2>";
         }
 
+        $output[] = "<table><thead><tr><th>Name</th><th>Value</th></tr></thead><tbody>";
+
         if ($serviceArray['phrase']) {
-            $output[] = ("<strong>phrase</strong>: " . $serviceArray['phrase']);
+            $output[] = ("<tr><th><strong>phrase</strong></th><td>" . $serviceArray['phrase'] . "</td></tr>");
         }
 
         if ($serviceArray['serviceid']) {
-            $output[] = ("<strong>servicename:</strong>" . $serviceArray['serviceid']);
+            $output[] = ("<tr><th><strong>servicename:</strong></th><td>" . $serviceArray['serviceid'] . "</td></tr>");
         } else {
-            $output[] = ("<strong>servicename:</strong> none");
+            $output[] = ("<tr><th><strong>servicename:</strong></th><td>none</td></tr>");
         }
 
         if ($serviceArray['service']) {
-            $output[] = ("<strong>class</strong>:" . get_class($serviceArray['service']));
+            $output[] = ("<tr><th><strong>class</strong></th><td>" . get_class($serviceArray['service']) . "</td></tr>");
         } else {
-            $output[] = ("<strong>class</strong>: none");
+            $output[] = ("<tr><th><strong>class</strong></th><td>none</td></tr>");
         }
-
+        $output[] = "</tbody></table>";
         return $output;
     }
 
